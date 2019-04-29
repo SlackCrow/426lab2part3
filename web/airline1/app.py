7a138412ccfdf3b4d83e1cf6edb7c35aa6e24f10
Jason Pettrone <jasonpettrone@gmail.com>
2019-04-28 21:58:29 -0400

    Can now move across webpages.

--- web/airline1/app.py
+++ web/airline1/app.py
@@ -3,11 +3,24 @@ from flask_pymongo import PyMongo
 import json
 
 app = Flask(__name__)
-
+app.config["MONGO_URI"] = 'mongodb+srv://lab2:ssHIbMqjtPAEGCLf@cluster0-moytw.mongodb.net/test?retryWrites=true'
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
+def home():
     return render_template("index.html")
+
+@app.route('/index.html')
+def index():
+    return render_template("index.html")
+
+@app.route('/reservation.html')
+def reservation():
+    return render_template("reservation.html")
+
+@app.route('/partners.html')
+def partners():
+    return render_template("partners.html")
 
 if __name__ == '__main__':
     app.run()
