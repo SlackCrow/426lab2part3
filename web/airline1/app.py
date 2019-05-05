bfcec5250424c66b66d54f4372917f25eebb227e
Jason Pettrone <jasonpettrone@gmail.com>
2019-05-05 17:09:11 -0400

    Fixed file handling (hopefully) and fixed port bug in airline2.

--- web/airline1/app.py
+++ web/airline1/app.py
@@ -6,7 +6,7 @@ from flask_restful import Resource, Api
 from requests import put, get
 from web3 import Web3, HTTPProvider, contract
 import json
-import os.path
+import os
 import time
 
 loginList = list()
@@ -21,7 +21,7 @@ airline1_private_key = "2CE8FABF78D208C16CC4C9A6A379AD83BD8AFAEB52B82CA918B4670D
 airline2_private_key = "197D0CA5903BD0DAF21076F09D1A6AC2D780D3010E531B5CFBB7CD7A69E275F1"
 infura_url = "https://ropsten.infura.io/v3/c5aa7254666449beb6f5a5d8536313dd"
 
-with open(os.path.dirname(__file__) + os.path.sep + '..' + os.path.sep + '..' + os.path.sep + 'smart_contract' + os.path.sep + 'abi.json') as f:
+with open(os.path.dirname(__file__) + os.sep + '..' + os.sep + '..' + os.sep + 'smart_contract' + os.sep + 'abi.json') as f:
     info_json = json.load(f)
 abi = info_json
 
--- web/airline2/app.py
+++ web/airline2/app.py
@@ -21,7 +21,7 @@ airline1_private_key = "2CE8FABF78D208C16CC4C9A6A379AD83BD8AFAEB52B82CA918B4670D
 airline2_private_key = "197D0CA5903BD0DAF21076F09D1A6AC2D780D3010E531B5CFBB7CD7A69E275F1"
 infura_url = "https://ropsten.infura.io/v3/c5aa7254666449beb6f5a5d8536313dd"
 
-with open(os.path.dirname(__file__) + os.path.sep + '..' + os.path.sep + '..' + os.path.sep + 'smart_contract' + os.path.sep + 'abi.json') as f:
+with open(os.path.dirname(__file__) + os.sep + '..' + os.sep + '..' + os.sep + 'smart_contract' + os.sep + 'abi.json') as f:
     info_json = json.load(f)
 abi = info_json
 
@@ -246,7 +246,7 @@ def changeAirline():
     #TODO:
     #if(result['result'] == 1):
         # Remove customer from DB
-        # Add customer to other airlines DB... I'm not sure how this is gonna work :(
+
 
     # Call response function in smart contract to put transaction on blockchain
     response_blockchain(airline2_wallet_addr, details, result)
